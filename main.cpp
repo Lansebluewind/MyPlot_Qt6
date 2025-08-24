5aabf67deb40a0ca0466dd791c75f2fac95acec6
Lansebluewind <102151986+Lansebluewind@users.noreply.github.com>
2025-08-24 20:25:09 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "test.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    test window;
+    window.show();
+    return app.exec();
+}
